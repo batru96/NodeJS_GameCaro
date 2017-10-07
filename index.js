5ce9c54e783c59ec824b5696af828a78aa8b1697
batru96 <gietbarca@gmail.com>
2017-10-07 17:36:40 +0700

    Update Sign in

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const md5 = require('md5');
 server.listen(3000, () => console.log('server is running'));
 
 app.get('/', (req, res) => console.log('Fun'));
-const usersOnline = [];
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -38,21 +37,49 @@ connection.connect((err) => {
             });
         });
         socket.on('USER_DANG_NHAP', info => {
-            const {email, password} = info;
-            const query = `SELECT * FROM USERS WHERE email = '${email}' AND password = '${md5(password)}'`;
+            const { email, password } = info;
+            const query = `SELECT * FROM USERS WHERE email = '${email}' AND password = '${md5(password)}' AND is_sign_in = 0`;
             connection.query(query, (err, result, fields) => {
                 if (err) {
                     console.log(err);
                 } else {
                     const arr = JSON.parse(JSON.stringify(result));
                     if (arr.length === 0) {
-                        socket.emit('SERVER_SEND_DANG_NHAP', 'DANG_NHAP_THAT_BAI');
+                        socket.emit('SERVER_SEND_DANG_NHAP_THAT_BAI', 'DANG_NHAP_THAT_BAI');
                     } else {
-                        socket.emit('SERVER_SEND_DANG_NHAP', 'DANG_NHAP_THANH_CONG');
+                        const { name } = arr[0];
+                        // Nếu đăng nhập thành công. Update 'is_sign_in = 1'.                        
+                        const queryUpdate = `UPDATE USERS SET is_sign_in = 1 WHERE email = '${email}'`;
+                        connection.query(queryUpdate, (err, result, fields) => {
+                            if (err) {
+                                console.log(err.message);
+                                return;
+                            }
+                            console.log('THANH_CONG');
+                        });
+
+                        // Lấy tất cả các user có 'is_sign_in = 1, gửi cho user vừa đăng nhập, kèm theo name và email'.
+                        const queryArr = `SELECT email, name FROM USERS WHERE is_sign_in = 1`;
+                        connection.query(queryArr, (err, result, fields) => {
+                            if (err) {
+                                console.log(err);
+                                return;
+                            }
+                            users = JSON.parse(JSON.stringify(result));
+                            socket.emit('SERVER_SEND_DANG_NHAP_THANH_CONG', { email, name, users });
+                        });
+
+                        // Gửi email và name của user mới đăng nhập cho các người còn lại
+                        socket.broadcast.emit('SERVER_SEND_USER_INFO', { email, name });
                     }
                 }
             });
         });
+    });
+
+    io.on('disconnect', () => {
+        // Update is_sign_in của user về 0.
+        // Gửi name và email cho tất cả các user còn lại.
     });
 });
 
